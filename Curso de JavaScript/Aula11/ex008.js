a6c6542d6caf98a732db75d013e403911842d0bd
Airk <w.airk@protonmail.com>
2020-06-11 21:00:11 -0300

    Aula do dia 11/06

---
+++ Curso de JavaScript/Aula11/ex008.js
@@ -0,0 +1,9 @@
+
+ vel = 60.01
+console.log(`Sua velocidade é: ${vel}`);
+
+if (vel >= 60){ //condição simples
+    console.log('MULTADO');
+}else{ //composta
+    console.log('Não ultrapasse o limite');
+}
